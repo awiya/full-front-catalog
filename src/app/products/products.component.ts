3b391258581c76837475fc4a0ebc740fba14105a
AYOUB <a.idihia@gmail.com>
2023-04-14 14:04:44 +0200

    same thing

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -38,4 +38,16 @@ export class ProductsComponent implements OnInit {
       },
     });
   }
+
+  handlePromoteProduct(p: Product) {
+    let promo = p.promotion;
+    this.productService.setPromotion(p.id).subscribe({
+      next: (data) => {
+        p.promotion = !promo;
+      },
+      error: (err) => {
+        this.errorMessage = err;
+      },
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Product } from '../model/product.model';
 
 @Injectable({
@@ -10,9 +10,9 @@ export class ProductService {
 
   constructor() {
     this.products = [
-      { id: 1, name: 'Computer', price: 650 },
-      { id: 2, name: 'Printer', price: 120 },
-      { id: 3, name: 'Smartphone', price: 300 },
+      { id: 1, name: 'Computer', price: 650, promotion: true },
+      { id: 2, name: 'Printer', price: 120, promotion: false },
+      { id: 3, name: 'Smartphone', price: 300, promotion: true },
     ];
   }
 
@@ -23,5 +23,14 @@ export class ProductService {
   deleteProduct(idToDelete: number): Observable<boolean> {
     this.products = this.products.filter((p) => p.id != idToDelete);
     return of(true);
+  }
+
+  setPromotion(idToSet: number): Observable<boolean> {
+    let produit = this.products.find((p) => p.id == idToSet);
+
+    if (produit) {
+      produit.promotion = !produit.promotion;
+      return of(true);
+    } else return throwError(() => new Error('Produit non trouvé!'));
   }
 }

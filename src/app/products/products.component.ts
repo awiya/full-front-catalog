ae9a67a1c288a2ce5783b1dd0819bfe682214f43
AYOUB <a.idihia@gmail.com>
2023-04-14 11:59:49 +0200

    Simple hard code pour afficher des produit dans un tableau

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Array<any>;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.products = [
+      { id: 1, name: 'Computer', price: 650 },
+      { id: 2, name: 'Printer', price: 120 },
+      { id: 3, name: 'Smartphone', price: 300 },
+    ];
+  }
+
+  handleDeleteProduct(p: any) {
+    let index = this.products.indexOf(p);
+    this.products.splice(index, 1);
+  }
 }
